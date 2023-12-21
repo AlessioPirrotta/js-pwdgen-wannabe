8dfe7321fff077d6031d85737ec7baad8305157a
AlessioPirrotta <alessiopirrotta92@gmail.com>
2023-12-21 20:24:34 +0100

    js exercise completed

---
+++ main.js
@@ -0,0 +1,10 @@
+let nomeUtente = prompt ("scrivi il tuo nome:")
+let cognomeUtente = prompt ("scrivi il tuo cognome:")
+let colorePreferitoUtente = prompt("scrivi il tuo colore preferito:")
+
+console.log(nomeUtente)
+console.log(cognomeUtente)
+console.log(colorePreferitoUtente)
+
+document.writeln("<br><br><strong>Password:  </strong>",nomeUtente, cognomeUtente, colorePreferitoUtente,"23" )
+
